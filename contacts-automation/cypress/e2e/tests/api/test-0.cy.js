6669660bdad8575898d0b92d7a7032fd0699e148
IdenTiclla <iden.ticlla@gmail.com>
2024-07-11 17:50:37 -0400

    adding new api route and test for that.

--- contacts-automation/cypress/e2e/tests/api/test-0.cy.js
+++ contacts-automation/cypress/e2e/tests/api/test-0.cy.js
@@ -13,4 +13,16 @@ describe('My test suite', () => {
             expect(data.Hello).to.be.eq('World')
         })
     })
+    it.only("My second test case.", () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://127.0.0.1:8000/name'
+        }).then(response => {
+            const data = response.body
+            expect(data).to.be.an('object')
+            expect(data).to.have.property('name')
+            expect(data.name).to.be.eq('Iden')
+            expect(data.name).to.be.an('string')
+        })
+    })
 })
